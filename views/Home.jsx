fade281d75256f70b7070d15d61f1b2bd3533c6b
Riley Timlin <RTimlin@allseasonmovers.com>
2023-04-19 14:00:54 -0400

    made api call

--- views/Home.jsx
+++ views/Home.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import {useEffect, useState} from 'react';
+import axios from "axios";
 
 const Home = () => {
+    const [weather, setWeather] = useState([]);
+    const getWeather = async () => {
+        const response = await axios.get('https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=&appid=13ef57263234e3613632e206c7dfd710')
+        const data = response;
+        setWeather(data);
+        console.log(weather);
+    }
+    useEffect(() => {
+        getWeather()
+    },[])
   return (
     <div>
         <nav>
@@ -10,6 +22,7 @@ const Home = () => {
 
         </nav>
         <h1>Welcome to the personal organizer app!</h1>
+        <h4>{weather.hourly.feels_like}</h4>
     </div>
   )
 }
