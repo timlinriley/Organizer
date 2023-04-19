7c00c9b0aada123754b411be4141ba551399022f
Riley Timlin <RTimlin@allseasonmovers.com>
2023-04-19 02:40:03 -0400

    fixed edit route

--- views/EditToDos.jsx
+++ views/EditToDos.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const EditToDos = (props) => {
   return (
     <div>
-        <form action={`/todos/${props.todo._id}?_metho=PUT`} method="POST">
+        <form action={`/todos/${props.todo._id}?_method=PUT`} method="POST">
         <input type="text" placeholder="Add a todo" name="item" />
         <input type="text" placeholder="Priority Level?" name="priority" />
         Completed: <input type="checkbox" name="completed" />
