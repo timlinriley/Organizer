bd7863a2b9d9cd7faee3261500af53593f6fef11
Riley Timlin <RTimlin@allseasonmovers.com>
2023-04-20 01:25:21 -0400

    styled add grocery view

--- views/NewGrocery.jsx
+++ views/NewGrocery.jsx
@@ -12,20 +12,42 @@ const NewGrocery = () => {
         paddingTop: "20px",
         fontSize: "50px"
     }
+    const formStyle = {
+        marginTop: "20px",
+        fontWeight: "bold",
+        border: "solid black",
+        padding: "10px"
+        
+    }
+    const inputStyle = {
+        margin: "8px"
+    }
+    const submitButton = {
+        padding: "10px",
+        border: "black",
+        borderRadius: "5%",
+        backgroundColor: "beige",
+        marginTop: "4px",
+        marginLeft: "20px",
+    }
+    const background = {
+        backgroundColor: "gray",
+        height: "400vh"
+    }
   return (
-    <div>
+    <div style={background}>
                 <nav style={navStyle}>
             <a href="/todos">Todos</a> 
             <a href="/groceries">Grocery List</a>
             <a href="/pantry">Pantry</a>
 
         </nav>
-            <h2>Add a new Grocery item to your list</h2>
-        <form action="/groceries" method="POST">
-            <input type="text" placeholder="Add a Grocery" name="product" />
-            <input type="text" placeholder="Specific Brand?" name="brand" />
-            <input type="number" placeholder="Quantity" name="quantity" />
-            <input type="submit" value="Add Grocery" />
+            <h2>Add a new Grocery item to your list:</h2>
+        <form style={formStyle} action="/groceries" method="POST">
+            <input style={inputStyle} type="text" placeholder="Add a Grocery" name="product" />
+            <input style={inputStyle} type="text" placeholder="Specific Brand?" name="brand" />
+            <input style={inputStyle} type="number" placeholder="Quantity" name="quantity" />
+            <input style={submitButton} type="submit" value="Add Grocery" />
         </form>   
     </div>
   )
