f5f3db62d0b2b3183a8e631f1b315a8c3280562c
Riley Timlin <RTimlin@allseasonmovers.com>
2023-04-20 00:43:49 -0400

    styled grocery list view page

--- views/GroceriesPage.jsx
+++ views/GroceriesPage.jsx
@@ -12,6 +12,47 @@ const GroceriesPage = (props) => {
         paddingTop: "20px",
         fontSize: "50px"
     }
+    const listItem = {
+        display: "flex",
+        padding: "10px",
+        margin: "8px"
+    }
+    const inputStyle = {
+        marginLeft: "5px",
+        marginRight: "5px",
+        padding: "10px",
+        fontWeight: "bold",
+    }
+    const deleteStyle = {
+        marginLeft: "5px",
+        marginRight: "5px",
+        padding: "10px",
+        fontWeight: "bold",
+        backgroundColor: "red"
+    }
+    const itemStyle = {
+        border: "solid black",
+        margin: "2px",
+        backgroundColor: "darkgray",
+        fontSize: "20px"
+  
+    }
+    const buttons = {
+        display: "flex",
+        alignSelf: "end",
+        marginLeft: "280px"
+    }
+    const spannedItem = {
+        fontWeight: "bold",
+        marginRight: "6px",
+        marginLeft: "10px"
+        
+    }
+    const add = {
+        marginLeft: "50px",
+        padding: "10px",
+        fontWeight: "bold"
+    }
   return (
     <div>
                 <nav style={navStyle}>
@@ -21,19 +62,23 @@ const GroceriesPage = (props) => {
 
         </nav>
             <h1>Grocery List</h1>
-     <button><a href="/groceries/new">Add a Grocery item to the list</a></button>
+     <button style={add}><a href="/groceries/new">Add a Grocery item to the list</a></button>
      <ul>
      {props.groceries.map((product, i)=>{
         return(
-        <li>Item: {product.product} {" "} Brand: {product.brand} {" "} Quantity: {product.quantity}
-        <button><a href={`/groceries/${product._id}/edit`}>Update</a></button>
+            <div style={itemStyle}>
+        <li style={listItem}><span style={spannedItem}>Item:</span> {product.product} {" "}<span style={spannedItem}> Brand:</span> {product.brand} {" "} <span style={spannedItem}>Quantity:</span> {" "}{product.quantity}
+            <div style={buttons}>
+        <button style={inputStyle}><a href={`/groceries/${product._id}/edit`}>Update</a></button>
         <form action={`/pantry/${product._id}`} METHOD="POST">
-            <input type="submit" value="Purchase" />
+            <input style={inputStyle} type="submit" value="Purchase" />
         </form>
         <form action={`/groceries/${product._id}?_method=DELETE`} METHOD="POST">
-            <input type="submit" value="DELETE" />
+            <input style={deleteStyle} type="submit" value="DELETE" />
         </form>
+        </div>
         </li>
+        </div>
         )
      })}
      </ul>
